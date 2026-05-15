d4eabc3f7d9fe2115ea4269bbf90de24abdcab6e
Kai <github@0902.one>
2026-05-15 22:08:37 +0800

    Update PCC favicon artwork

--- pcc-static/pcc-enhancements.js
+++ pcc-static/pcc-enhancements.js
@@ -9,7 +9,7 @@
   const RESULT_META_SELECTOR = "#resultMeta";
   const ORG_INPUT_SELECTOR = "#orgName";
   const SEARCH_BUTTON_SELECTOR = "#searchButton";
-  const ENHANCEMENT_VERSION = "20260514-12";
+  const ENHANCEMENT_VERSION = "20260515-1";
   let amountSortApplying = false;
 
   function readHistory() {
--- pcc-static/sw.js
+++ pcc-static/sw.js
@@ -1,4 +1,4 @@
-const ASSET_VERSION = "20260514-12";
+const ASSET_VERSION = "20260515-1";
 const CACHE_NAME = `pcc-tender-${ASSET_VERSION}`;
 const APP_SHELL = [
   "/pcc/",
