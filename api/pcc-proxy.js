915ab002fafc082d10e695f2b78077bf77a4a0f6
Kai <github@0902.one>
2026-06-14 10:31:17 +0800

    Use dedicated PCC NAS subdomain

--- api/pcc-proxy.js
+++ api/pcc-proxy.js
@@ -1,6 +1,6 @@
-const TARGET_ORIGIN = "http://nas220.i234.me:55721";
+const TARGET_ORIGIN = "http://pcc.nas220.i234.me";
 const PUBLIC_BASE = "/pcc";
-const ASSET_VERSION = "20260614-1";
+const ASSET_VERSION = "20260614-2";
 const PWA_HEAD = `
     <meta name="theme-color" content="#0f766e">
     <meta name="mobile-web-app-capable" content="yes">
